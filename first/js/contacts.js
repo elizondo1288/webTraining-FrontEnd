4399e845e20066cd8ed8b2a2637ee7e0bfb712c4
Esteban <elizondo1288@gmail.com>
2015-06-04 17:37:25 -0600

    changes adding bower and finishing the CRUD

--- first/js/contacts.js
+++ first/js/contacts.js
@@ -1,13 +1,43 @@
 (function(){
     
     var model = {
-            contacts: [{ name: "Esteban Elizondo", address: "San Jose Costa Rica", email:"elizondo1288@hotmail.com",phone:"8864-1981"},
-                    { name: "Pedro Perez", address: "Cartago", email:"pedrop@gmail.com",phone:"8832-1123"}]
+            contacts: [{ name: "Esteban Elizondo", address: "San Jose Costa Rica", email:"elizondo1288@hotmail.com",phone:"8864-1981",updating:false},
+                    { name: "Pedro Perez", address: "Cartago", email:"pedrop@gmail.com",phone:"8832-1123",updating:false}]
         };
 
     //we set the module for the angular page
     var contactsApp = angular.module("contactsApp", []);
 
+
+    /*contactsApp.filter('searchFor', function(){
+
+        // All filters must return a function. The first parameter
+        // is the data that is to be filtered, and the second is an
+        // argument that may be passed with a colon (searchFor:searchString)
+
+        return function(arr, searchString){
+
+            if(!searchString){
+                return arr;
+            }
+
+            var result = [];
+
+            searchString = searchString.toLowerCase();
+
+            // Using the forEach helper method to loop through the array
+            angular.forEach(arr, function(item){
+
+                if(item.title.toLowerCase().indexOf(searchString) !== -1){
+                    result.push(item);
+                }
+
+            });
+
+            return result;
+        };
+
+    });*/
     /*
     * We set the controller for this contact application
     */
@@ -29,21 +59,25 @@
         };
 
         $scope.addNewContact = function (nam, add, em, ph) {
-                $scope.model.contacts.push({ name: nam, address: add, email:em, phone:ph});
+                $scope.model.contacts.push({ name: nam, address: add, email:em, phone:ph, updating:false});
                 $scope.wasInserted = true;
-                $scope.nameF='';
-                $scope.adressF='';
-                $scope.emailF='';
-                $scope.phoneF='';
-                $scope.addContactForm.$setPristine();
-                $scope.addContactForm.$setUntouched();
-                
+                $scope.clearInsertForm();
                 
                 
         };
 
-        $scope.clearInsertForm = function(){
+        $scope.deleteContact = function(item){
+            var index=$scope.model.contacts.indexOf(item)
+            $scope.model.contacts.splice(index,1);
+        };
 
+        $scope.updateContact = function(item){
+
+            if(item.updating){
+                item.updating = false;
+            } else{
+                item.updating = true;
+            }
         }
 
         $scope.presentDiv = function (num) {
@@ -52,12 +86,22 @@
             if (num == 1) {
                 $scope.visibililtyAdd = false;
                 $scope.visibililtyList = true;
+                $scope.clearInsertForm();
             }else{
                 $scope.visibililtyAdd = true;
                 $scope.visibililtyList = false;
             };
 
             $scope.wasInserted = false;
+        };
+
+        $scope.clearInsertForm = function(){
+            $scope.nameF='';
+            $scope.adressF='';
+            $scope.emailF='';
+            $scope.phoneF='';
+            $scope.addContactForm.$setPristine();
+            $scope.addContactForm.$setUntouched();
         };
 
     });//end of controller
